10aa86f803fc187ac93ecd07fcd54b5c3a9172e1
Marcos Sousa <marcosviniciocardoso123@gmail.com>
2024-05-21 18:27:43 -0300

    implementação: configuração do pré processador css stylus.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,5 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  TESTE
   <RouterView />
 </template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './assets/global.styl'
 
 import { createApp } from 'vue'
 import App from './App.vue'
