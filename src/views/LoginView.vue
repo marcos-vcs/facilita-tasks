c212b65291a911dcd729429b5fe49f18501b48e2
Marcos Sousa <marcosviniciocardoso123@gmail.com>
2024-05-21 17:49:16 -0300

    implementação: configuração do vite para buscar o título da página do package.json, remoção de componentes e css default e configuração da fonte padrão da aplicação.

---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'LoginView',
+    data() {
+
+    },
+    methods: {
+
+    }
+})
+</script>
+
+<template>
+    <main>
+
+    </main>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import { readFileSync } from 'fs'
+import path from 'path'
+
+// eslint-disable-next-line no-undef
+const packageJson = JSON.parse(readFileSync(path.resolve(__dirname, 'package.json'), 'utf-8'))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: packageJson.name
+        }
+      }
+    })
   ],
   resolve: {
     alias: {
